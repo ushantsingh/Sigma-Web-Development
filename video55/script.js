c89730dcf7fcfa389c5483ff9ec6ba9029a15c71
Ushant Singh <ushantsingh4@gmail.com>
2025-11-24 00:52:35 +0530

    JAVAScript variable

---
+++ video55/script.js
@@ -0,0 +1,50 @@
+console.log("Hey this is 55th tutorial");
+var a = 5;
+a = a + 1;
+let b = 6;
+let c = "Ushant";
+let _a = "Shubham";
+// var 55a = "Rohan";  --> not allowed 
+console.log(a+b+8);
+console.log(typeof a,typeof b, typeof c);
+
+{
+    let a = 66;
+    console.log(a);
+}
+
+    console.log(a);
+    
+{
+    var a = 66;
+    console.log(a);
+    
+}
+    console.log(a);
+    
+
+// const a1 = 6;
+// a1 = a1 + 1;   --> because a1 is const 
+
+let x = "Ushant Bhai";
+let y = 33;
+let z = 3.55;
+const p = true;
+let q = undefined;
+let r = null;
+
+console.log(x, y, z, p, q, r);
+console.log(typeof x, typeof y, typeof z, typeof p, typeof q, typeof r);
+
+let o = {
+    "name": "Ushant",
+    "Job code": 5445,
+    "is Handsome":true
+}
+
+console.log(o);
+o.salary="100cr"
+console.log(o);
+o.salary="500cr"
+console.log(o);
+
