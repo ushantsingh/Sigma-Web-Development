883b5c0eab8138fb07a3dcfb42e21c1a5b43cc08
Ushant Singh <ushantsingh4@gmail.com>
2025-11-24 01:38:11 +0530

    if else

---
+++ video56/stricp.js
@@ -0,0 +1,44 @@
+console.log("Hello i am conditional Tutorial ");
+let age = 0;
+// let grace = 2;
+
+// age += grace;
+// console.log(age);
+
+
+// console.log(age+grace);
+// console.log(age-grace);
+// console.log(age*grace);
+// console.log(age/grace);
+// console.log(age**grace);
+// console.log(age%grace);
+
+// if(age+grace > 18) {
+//     console.log("You can drive ");
+// }
+// else {
+//     console.log("You cannot drive");
+    
+// }
+
+// if(age == 18) {
+//     console.log("You can drive ");
+// }
+// else {
+//     console.log("You cannot drive");
+    
+// }
+// if(age == 18) {
+//     connsole.log("You can drive ");
+// }
+// else if (age == 0) {
+//     console.log("Are you kidding?");
+// }
+// else {
+//     console.log("You cannot drive");   
+// }
+
+ a = 6;
+ b = 8;
+let c = a > b ? (a - b) : (b - a);
+
