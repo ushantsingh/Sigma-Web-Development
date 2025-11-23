fb0d97c364fce8ea2361c743cd6d26f525bbb806
Ushant Singh <ushantsingh4@gmail.com>
2025-11-24 02:19:55 +0530

    Loops

---
+++ video57/index.js
@@ -0,0 +1,40 @@
+console.log("I am a tutorial on loops");
+// let a = 1;
+// for (let i = 0; i < 100; i++) {
+//     console.log(a+i);
+// }
+
+// let obj = {
+//     "name": "Ushant",
+//     "role": "Programmer",
+//     "company":"CodeWithUshant AI"
+// }
+
+// // for off
+// for (const key in obj) {
+//     const element = obj[key];
+//     console.log(key,element);
+// }
+
+// // for in
+
+// for (const element of "Ushant") {
+//     console.log(element);
+    
+// }
+
+// while loop
+
+// let i = 0;
+// while (i < 6) {
+//     console.log(i);
+//     i++;
+// }
+
+// do while loop
+
+let i = 10;
+do {
+    console.log(i);
+    i++;
+}while (i < 6);
