cf130024c38f110cd5dcb3d21d13277b39e8ccdd
Ushant Singh <ushantsingh4@gmail.com>
2025-11-25 01:46:06 +0530

    String

--- video58/index.js
+++ video58/index.js
@@ -1,17 +1,17 @@
-// console.log("Hey ushant you are nice!");
-// console.log("Hey ushant you are good!");
-// console.log("Hey ushant your tshirt is nice!");
-// console.log("Hey ushant your course is good too!");
+console.log("Hey ushant you are nice!");
+console.log("Hey ushant you are good!");
+console.log("Hey ushant your tshirt is nice!");
+console.log("Hey ushant your course is good too!");
 
-// function name(name) {
-//     console.log("Hey "+name+" you are nice!");
-//     console.log("Hey "+name+" you are good!");
-//     console.log("Hey "+name+" your tshirt is nice!");
-//     console.log("Hey "+name+" ushant your course is good too!");
-// }
-// name("ushant")
-// name("arya")
-// name("tishya")
+function name(name) {
+    console.log("Hey "+name+" you are nice!");
+    console.log("Hey "+name+" you are good!");
+    console.log("Hey "+name+" your tshirt is nice!");
+    console.log("Hey "+name+" ushant your course is good too!");
+}
+name("ushant")
+name("arya")
+name("tishya")
 
 function sum(a,b) {
     // console.log(a+b);
@@ -19,18 +19,18 @@ function sum(a,b) {
 }
 
 
-// result1=sum(3,5)
-// result2=sum(7,5)
-// result3=sum(3,13)
-// result4=sum(3)
+result1=sum(3,5)
+result2=sum(7,5)
+result3=sum(3,13)
+result4=sum(3)
 
-// console.log("The sum of ythese number is : "+result1);
-// console.log("The sum of ythese number is : "+result2);
-// console.log("The sum of ythese number is : "+result3);
-// console.log("The sum of ythese number is : "+result4);
+console.log("The sum of ythese number is : "+result1);
+console.log("The sum of ythese number is : "+result2);
+console.log("The sum of ythese number is : "+result3);
+console.log("The sum of ythese number is : "+result4);
 
 
-// arraow fuhnction
+arraow fuhnction
 
 const func1 = (x) => {
     console.log("I am an Arrow Function",x);
---
+++ video60/index.js
@@ -0,0 +1,26 @@
+console.log("this is string tutorial ");
+let a = "ushant";
+console.log(a[0]);
+console.log(a[1]);
+console.log(a[2]);
+console.log(a[3]);
+console.log(a[4]);
+console.log(a[5]);
+// console.log(a[6]);
+
+console.log(a.length);
+
+let name ="Ushant"
+let friend = "Arya"
+console.log("His name is "+name+" and his friends name is "+friend);
+console.log(`His name is ${name} and his friends name is ${friend}`); //---> Template Literals
+
+let b = "Tishya";
+console.log(b.toLowerCase());
+console.log(b.toUpperCase());
+console.log(b.slice(1,3));
+console.log(b.slice(1));
+console.log(b.replace("sh",45));
+console.log(b.concat(a,"Aditya","Rahul","priya"));
+
+console.log(b);
