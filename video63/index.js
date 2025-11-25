3905df384fcf00db9334eff49a624b1a048dc2ed
Ushant Singh <ushantsingh4@gmail.com>
2025-11-26 02:05:18 +0530

    Video 63

---
+++ video63/index.js
@@ -0,0 +1,25 @@
+let arr = [1, 2, 3, 4, 5, 6, 7]
+let a1 = [3, 7, 53]
+let a2=[8,6,3]
+
+console.log(arr);
+arr[0]=5666
+// console.log(arr.length);
+// console.log(arr[0]);
+// console.log(arr[2]);
+// console.log(arr[4]);
+// console.log(arr.toString());
+// console.log(arr.join(" and "));
+// console.log(arr.pop());
+// console.log(arr.push("Ushant"));
+// console.log(arr);
+// console.log(arr.shift());
+// console.log(arr.unshift("Jack"));
+// console.log(arr);
+// delete arr[6]
+// console.log(arr);
+// console.log(arr.length);
+// console.log(arr.concat(a1,a2));
+console.log(arr.splice(1,3));
+console.log(arr);
+
---
+++ video63/loops.js
@@ -0,0 +1,35 @@
+let a=[1,93,5,6,88]
+
+// for (let index = 0; index < a.length; index++){
+//     const element = a[index]
+//     console.log(element);
+    
+// }
+
+// a.forEach((value, index, arr) => {
+//     console.log(value,index,arr);
+    
+// })
+
+// for in loop
+
+// let obj = {
+//     a: 1,
+//     b: 2,
+//     c:3
+// }
+
+// for (const key in obj) {
+//     if (Object.hasOwnProperty.call(obj, key)) {
+//         const element = obj[key];
+//         console.log(key,element);
+        
+//     }
+// }
+
+// for off loop
+
+for (const element of a) {
+    console.log(element);
+    
+}
