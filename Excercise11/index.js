dff15c0b1cfd66b4ef6292aa7a759b75b3aff8cb
Ushant Singh <ushantsingh4@gmail.com>
2025-11-27 03:05:28 +0530

    Exercise 12

--- Excercise11/index.js
+++ Excercise11/index.js
@@ -1,9 +1,17 @@
 // Factorial 
 
-let n=5
-let fact = 1
+// let n=5
+// let fact = 1
 
-for (let index = 1; index <= n; index++) {
-    fact = fact * index;
+// for (let index = 1; index <= n; index++) {
+//     fact = fact * index;
+// }
+// console.log(fact);
+
+
+let a = 0;
+function factorial(number) {
+    let arr = Array.from(Array(number + 1).keys())
+    let c = arr.slice(1,).reduce((a, b) => a * b)
+    return c
 }
-console.log(fact);
