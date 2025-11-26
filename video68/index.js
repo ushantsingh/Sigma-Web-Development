6d34cc06999caee2afab42097bebab0b765f9b9a
Ushant Singh <ushantsingh4@gmail.com>
2025-11-27 02:55:24 +0530

    video68

---
+++ video68/index.js
@@ -0,0 +1,30 @@
+console.log("Ushant");
+// let boxes = document.getElementsByClassName("box")
+// console.log(boxes);
+
+// boxes[2].style.backgroundcolor="red"
+
+// document.getElementById("redbox").style.backgroundColor="red"
+
+// document.querySelector(".box").style.backgroundColor="green"
+
+// console.log(document.querySelectorAll(".box"))
+
+// document.querySelectorAll(".box").forEach(e => {
+//     e.style.backgroundColor = "green";
+// })
+
+// document.getElementsByTagName("div")
+
+// e=document.getElementsByTagName("div")
+// HTMLCollection(6) [div.container, div.box, div#redbox.box, div.box, div.box, div.box, redbox: div#redbox.box]
+// e[4].matches("#redbox")
+// false
+// e[3].matches("#redbox")
+// false
+// e[3].closest("#redbox")
+// null
+// e[3].closest(".container")
+// <div class=​"container">​…​</div>​
+// e[3].closest("html")
+// <html lang=​"en">​scroll<head>​…​</head>​<body>​…​</body>​</html>​
