088a1a3d4cbed72e063a253d9087e9922f83e7ea
Ushant Singh <ushantsingh4@gmail.com>
2025-12-06 16:03:31 +0530

    Excercise 13

--- Excercise 13/script.js
+++ Excercise 13/script.js
@@ -1,3 +1,29 @@
 function createCard(title, cName, views, monthsOld, Duration, thumbnail) {
+
+    let viewStr
+    if (views < 1000000) {
+        viewStr = views / 1000 + "k";
+    } else if (views > 1000000) {
+        viewStr = views / 1000000 + "M";
+    } else {
+        viewStr = views / 1000 + "k";
+    }
+
+    let html=`<div class="card">
+            <div class="image">
+                <img src="${thumbnail}"
+                    alt="">
+                <div class="capsule">${Duration}</div>
+            </div>
+            <div class="text">
+                <h1>${title}</h1>
+                <p>${cName} . ${viewStr} views . ${monthsOld} months ago</p>
+            </div>
+        </div>`
     
+    document.querySelector(".container").innerHTML=document.querySelector(".container").innerHTML+html
 }
+
+
+createCard("Introduction to Backend | Sigma Web Dev video #2", "CodeWithHarry", 560000000, 7, "31:22", "https://i.ytimg.com/vi/tVzUXW6siu0/hqdefault.jpg?sqp=-oaymwEcCPYBEIoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLACwWOixJVrKLFindK92kYMgTcQbw")
+
