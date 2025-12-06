2fe976897e026cde2e04490056c1ba60ab96ef4d
Ushant Singh <ushantsingh4@gmail.com>
2025-12-06 19:35:00 +0530

    Question 7

---
+++ interview Questions/Question_7.js
@@ -0,0 +1,14 @@
+// The Local Storage Manager: You are working on a note-taking app, and you want to implement a function named saveNoteToLocalStorage that takes a note object and saves it to the browser's local storage.
+
+function saveNoteToLocalStorage(note) {
+    localStorage.setItem("note", note);
+}
+
+let note = localStorage.getItem("note")
+document.querySelector(".note").innerHTML=(note)
+document.querySelector("button").addEventListener
+    ("click", () => {
+        let note = prompt("Enter your note")
+        saveNoteToLocalStorage(note)
+        document.querySelector(".note").innerHTML=(note)
+})
